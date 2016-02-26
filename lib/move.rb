e0e026d810792301172f0a1fcfe9950fe79213ca
Ellie Spinney <ellespinney@gmail.com>
2016-02-26 17:53:58 +0000

    nitrous-backup at Fri Feb 26 17:53:58 UTC 2016

--- lib/move.rb
+++ lib/move.rb
@@ -4,6 +4,15 @@ def display_board(board)
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+
+end
+def update_array_at_with(array, position, value)
+  array[position] = value
+# code your move method here!
+end
+def move(board, position, X)
+X = "X"
+  board[] = #to_i+1
+
 end
 
-# code your move method here!
